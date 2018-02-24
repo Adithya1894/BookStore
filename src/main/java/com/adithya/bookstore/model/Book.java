ada8e653737ecf49068161c828fe74fc07c39ccb
adithya1894 <adithya1894@gmail.com>
2018-02-24 03:45:12 -0500

    added the annotations for the Entity class

--- src/main/java/com/adithya/bookstore/model/Book.java
+++ src/main/java/com/adithya/bookstore/model/Book.java
@@ -1,8 +1,6 @@
 package com.adithya.bookstore.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -11,27 +9,112 @@ public class Book {
     @Id @GeneratedValue
     private Long id;
 
+    @Column(length = 200)
     private String title;
 
+    @Column(length = 1000)
     private String description;
 
+    @Column(name = "unit_cost")
     private float unitcost;
 
     private String isbn;
 
+    @Column(name = "published_date")
+    @Temporal(TemporalType.DATE)
     private Date publishedDate;
 
+    @Column(name = "no_of_pages")
     private Integer noOfPages;
 
+    private String image;
 
+    private Language language;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public float getUnitcost() {
+        return unitcost;
+    }
 
+    public void setUnitcost(float unitcost) {
+        this.unitcost = unitcost;
+    }
 
+    public String getIsbn() {
+        return isbn;
+    }
 
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public Integer getNoOfPages() {
+        return noOfPages;
+    }
+
+    public void setNoOfPages(Integer noOfPages) {
+        this.noOfPages = noOfPages;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitcost=" + unitcost +
+                ", isbn='" + isbn + '\'' +
+                ", publishedDate=" + publishedDate +
+                ", noOfPages=" + noOfPages +
+                ", image='" + image + '\'' +
+                ", language=" + language +
+                '}';
+    }
 }
