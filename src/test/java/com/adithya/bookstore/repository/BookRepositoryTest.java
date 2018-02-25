b9fa611158429b1ef783c8d2b569c1c346bc2243
adithya1894 <adithya1894@gmail.com>
2018-02-24 20:16:40 -0500

    added the arqualian integration test

---
+++ src/test/java/com/adithya/bookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.adithya.bookstore.repository;
+
+import com.adithya.bookstore.model.Book;
+import com.adithya.bookstore.model.Language;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class BookRepositoryTest {
+
+    @Inject
+    private BookRepository bookRepository;
+
+
+    @Test
+    public void create() throws Exception{
+
+        assertEquals(Long.valueOf(0), bookRepository.countAll());
+        assertEquals(0, bookRepository.findAll().size());
+
+        //create a book
+
+        Book book = new Book("isbn", )
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(BookRepository.class)
+                .addClass(Book.class)
+                .addClass(Language.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource("META-INF/test-persistence.xml", "persistence.xml");
+    }
+
+}
