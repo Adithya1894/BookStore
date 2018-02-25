efdba57e4eccb865073adaea4bf2c61771cccbfb
adithya1894 <adithya1894@gmail.com>
2018-02-25 13:43:45 -0500

    added the Rest Endpoints for Get, Post and Delete

--- src/main/java/com/adithya/bookstore/rest/BookEndPoint.java
+++ src/main/java/com/adithya/bookstore/rest/BookEndPoint.java
@@ -4,11 +4,13 @@ import com.adithya.bookstore.model.Book;
 import com.adithya.bookstore.repository.BookRepository;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.validation.constraints.Min;
+import javax.ws.rs.*;
 //import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -16,19 +18,34 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path("/books")
 public class BookEndPoint {
 
-    public Book getBook(Long id) {
-        return bookRepository.find(id);
+    @GET
+    @Path("{id: \\d+}")
+    public Response getBook(@PathParam("id")  @Min(1) Long id) {
+
+        Book book = bookRepository.find(id);
+        if (book == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        return Response.ok(book).build();
     }
 
-
-    public Book createBook(Book book) {
-        return bookRepository.create(book);
+    @POST
+    @Consumes(APPLICATION_JSON)
+    public Response createBook(Book book, @Context UriInfo uriInfo) {
+        book = bookRepository.create(book);
+        URI createdURI = uriInfo.getBaseUriBuilder().path(book.getId().toString()).build();
+        return Response.created(createdURI).build();
     }
 
-    public void deleteBook(Long id) {
+    //Delete request to delete the particular book using its id.
+    @DELETE
+    @Path("/{id : \\d+}")
+    public Response deleteBook(@PathParam("id") @Min(1) Long id) {
         bookRepository.delete(id);
+        return Response.noContent().build();
     }
 
+    //Get request to pull the total books
     @GET
     @Produces(APPLICATION_JSON)
     public Response getBooks() {
@@ -41,8 +58,16 @@ public class BookEndPoint {
         return Response.ok(books).build();
    }
 
-    public Long countBook() {
-        return bookRepository.countAll();
+    //Get request to count the totatl books available
+    @GET
+    @Path("/count")
+    @Produces(APPLICATION_JSON)
+    public Response countBook() {
+        Long noOfBooks = bookRepository.countAll();
+        if(noOfBooks == 0)
+            return Response.noContent().build();
+
+        return Response.ok(noOfBooks).build();
     }
 
     @Inject
