d51bcb05169cb6649dde6327b3216779896e3f5d
adithya1894 <adithya1894@gmail.com>
2018-02-26 19:26:48 -0500

    Added the Endpoint annotaions for swagger and built swagger contracts

--- src/main/java/com/adithya/bookstore/rest/BookEndPoint.java
+++ src/main/java/com/adithya/bookstore/rest/BookEndPoint.java
@@ -2,11 +2,14 @@ package com.adithya.bookstore.rest;
 
 import com.adithya.bookstore.model.Book;
 import com.adithya.bookstore.repository.BookRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import javax.inject.Inject;
 import javax.validation.constraints.Min;
 import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -16,11 +19,21 @@ import java.util.List;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("/books")
+@Api("Books")
 public class BookEndPoint {
 
     @GET
     @Path("{id: \\d+}")
     @Produces(APPLICATION_JSON)
+    @ApiOperation(value = "Returns a book if it is available",response = Book.class)
+    @ApiResponses({
+
+            @ApiResponse(code = 200, message = "Book Found"),
+            @ApiResponse(code = 400, message = "Bad request or invalid data"),
+            @ApiResponse(code = 204, message = "no content"),
+            @ApiResponse(code = 404, message = "book not found")
+
+    })
     public Response getBook(@PathParam("id")  @Min(1) Long id) {
 
         Book book = bookRepository.find(id);
@@ -32,6 +45,11 @@ public class BookEndPoint {
 
     @POST
     @Consumes(APPLICATION_JSON)
+    @ApiOperation(value = "Creates a book with the given details after validation")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Book has been created"),
+            @ApiResponse(code = 415, message = "Format Incorrect")
+    })
     public Response createBook(Book book, @Context UriInfo uriInfo) {
         book = bookRepository.create(book);
         URI createdURI = uriInfo.getBaseUriBuilder().path(book.getId().toString()).build();
@@ -42,6 +60,11 @@ public class BookEndPoint {
     @DELETE
     @Path("/{id : \\d+}")
     @Produces(APPLICATION_JSON)
+    @ApiOperation(value = "Deletes a book with Delete request")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Book Deleted successfully"),
+            @ApiResponse(code = 500, message = "Internal Error")
+    })
     public Response deleteBook(@PathParam("id") @Min(1) Long id) {
         bookRepository.delete(id);
         return Response.noContent().build();
@@ -50,6 +73,15 @@ public class BookEndPoint {
     //Get request to pull the total books
     @GET
     @Produces(APPLICATION_JSON)
+    @ApiOperation(value = "Returns a list of books if  available",response = Book.class)
+    @ApiResponses({
+
+            @ApiResponse(code = 200, message = "Book Found"),
+            @ApiResponse(code = 204, message = "no content"),
+            @ApiResponse(code = 400, message = "Bad request or invalid data"),
+            @ApiResponse(code = 404, message = "book not found")
+
+    })
     public Response getBooks() {
 
         List<Book> books = bookRepository.findAll();
@@ -64,6 +96,11 @@ public class BookEndPoint {
     @GET
     @Path("/count")
     @Produces(APPLICATION_JSON)
+    @ApiOperation(value = "Counts the number of books")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "books found"),
+            @ApiResponse(code = 204, message = "No Content")
+    })
     public Response countBook() {
         Long noOfBooks = bookRepository.countAll();
         if(noOfBooks == 0)
