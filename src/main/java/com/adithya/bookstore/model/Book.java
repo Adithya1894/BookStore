4d20e75195a01fa8ecdc8f3ff7c4d3780d4a22f0
adithya1894 <adithya1894@gmail.com>
2018-02-26 19:25:46 -0500

    Added the swagger annotations

--- src/main/java/com/adithya/bookstore/model/Book.java
+++ src/main/java/com/adithya/bookstore/model/Book.java
@@ -1,5 +1,8 @@
 package com.adithya.bookstore.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -8,48 +11,58 @@ import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
+@ApiModel(description = "Book Resource")
 public class Book {
     //default constructor
     public Book() {
     }
 
     @Id @GeneratedValue
+    @ApiModelProperty("Primary Identifier")
     private Long id;
 
 
     @Column(length = 200)
     @NotNull
     @Size(min=1, max = 200 )
+    @ApiModelProperty("title")
     private String title;
 
     @Column(length = 1000)
     @Size(min=1, max = 100000)
+    @ApiModelProperty("Description")
     private String description;
 
     @Column(name = "unit_cost")
     @Min(1)
+    @ApiModelProperty("Cost")
     private float unitcost;
 
     @NotNull
     @Size(min =1, max = 50)
+    @ApiModelProperty("ISBN")
     private String isbn;
 
     @Column(name = "publication_date")
     @Temporal(TemporalType.DATE)
     @Past
+    @ApiModelProperty("Published_Date")
     private Date publishedDate;
 
     @Column(name = "nb_of_pages")
+    @ApiModelProperty("PageCount")
     private Integer noOfPages;
 
+    @ApiModelProperty("Image Url")
     private String image_url;
 
+    @ApiModelProperty("language")
     private Language language;
 
     public Book(String id, String isbn, String title, String image_url,Language language,float unit_cost,Integer noOfPages, Date publishedDate, String description) {
         this.title = title;
         this.description = description;
-        this.unitcost = unitcost;
+        this.unitcost = unit_cost;
         this.isbn = isbn;
         this.publishedDate = publishedDate;
         this.noOfPages = noOfPages;
